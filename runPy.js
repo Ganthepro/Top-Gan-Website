09f77624a36bac49f6fe2dc5465a1fcfe173d176
Tamtikorn <gan_zaza_12@hotmail.com>
2023-03-09 11:16:34 +0700

    new

--- runPy.js
+++ runPy.js
@@ -1,35 +1,62 @@
-const { spawn } = require('child_process');
 const express = require('express');
+const multer = require('multer');
 const path = require('path');
-const app = express()
+const { spawn } = require('child_process');
 
+const app = express();
+let extname = null
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: function(req, file, cb) {
+    cb(null, "test" + path.extname(file.originalname));
+    extname = path.extname(file.originalname)
+  }
+});
 const pythonProcess = spawn('python', ['upscale_color.py']);
+// Initialize upload
+const upload = multer({
+  storage: storage
+}).single('picture');
 
 app.get("/",(req,res) => {
-  res.type("text/html")
-  res.sendFile(path.join(__dirname,"test.html"))
-})
+    res.type("text/html")
+    res.sendFile(path.join(__dirname,"test.html"))
+  })
+// Route for uploading picture
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(req.file);
+      console.log('Picture uploaded!');
+      res.redirect("http://localhost:5500/runPy")
+    }
+  });
+});
 
-app.get("/runPy", (req, res) => {
+app.get("/runPy", (req,res) => {
   // const fname = req.params.fname
   let count = 0;
-  try {
-    pythonProcess.stdout.on('data', (data) => {
-      if (count == 0) {
-        res.send(data.toString());
-      }
-      count++
-    });
-    
-    pythonProcess.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-    });
-    
-    pythonProcess.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-    })
-    pythonProcess.stdin.write("6.tif" + "\n")
-  } catch {}
+  pythonProcess.stdout.on('data', (data) => {
+    if (count == 0) {
+      res.send(data.toString());
+    }
+    count++
+  });
+  
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  
+  pythonProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  })
+  pythonProcess.stdin.write("uploads/test" + extname + "\n")
 })
 
-app.listen(5500, () => console.log('Server running on port 5500'));
+// Start server
+app.listen(5500, () => {
+  console.log('Server started on port 5500');
+});
---
+++ test.js
@@ -0,0 +1,50 @@
+const { spawn } = require('child_process');
+const express = require('express');
+const path = require('path');
+const app = express()
+const multer = require('multer');
+
+const pythonProcess = spawn('python', ['upscale_color.py']);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, "img")
+  }
+});
+const upload = multer({ storage: storage });
+
+app.get("/",(req,res) => {
+  res.type("text/html")
+  res.sendFile(path.join(__dirname,"test.html"))
+})
+
+app.post('/upload', upload.single('image'), function(req, res) {
+  console.log('File uploaded successfully!')
+  // res.redirect("http://localhost:5500/runPy")
+})
+
+app.get("/runPy", (req, file ,res) => {
+  // const fname = req.params.fname
+  let count = 0;
+  try {
+    pythonProcess.stdout.on('data', (data) => {
+      if (count == 0) {
+        res.send(data.toString());
+      }
+      count++
+    });
+    
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+    
+    pythonProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    })
+    pythonProcess.stdin.write("uploads/img.png" + "\n")
+  } catch {}
+})
+
+app.listen(5500, () => console.log('Server running on port 5500'));
